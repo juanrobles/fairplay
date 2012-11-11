826bfbd597def8038e8f0cac3ff43c3d5586bbb5
Juan Robles <juan.robles@sage.com>
2012-11-11 17:42:22 +0000

    Table is now properly built using hashes

--- lib/table_builder.rb
+++ lib/table_builder.rb
@@ -7,6 +7,23 @@ class TableBuilder
   # Initializes the object.
   # @param fair_play [String] expects a fairplay compliant key
   def initialize(fairplay_key)
+    @table = {}
+    build_table(fairplay_key)
+  end
+
+  private
+
+  def build_table(fairplay_key)
+    string_table = fairplay_key.scan(/\w{5}/)
+    string_table.each_index do |row_index|
+      string_row = string_table[row_index]
+      col_pos = 0
+      string_row.each_char do |item_index|
+        letter = string_row[col_pos]
+        self.table[letter] = { x: col_pos, y: row_index}
+        col_pos += 1
+      end
+    end
   end
 
 end
--- spec/lib/table_builder_spec.rb
+++ spec/lib/table_builder_spec.rb
@@ -6,7 +6,16 @@ describe TableBuilder do
   let(:phrase) { "Playfajr example!" }
   let(:key_builder) { KeyBuilder.new phrase }
 
+  subject { described_class.new key_builder.key}
+
   describe :table do
+    it 'should have a table' do
+      subject.table.should be
+    end
+
+    it 'should contain 25 elements' do
+      subject.table.size.should == 25
+    end
 
   end
 
